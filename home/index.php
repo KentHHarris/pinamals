32d5394d8a4b516ef6f44c9f749b1981c7774008
KentHHarris <kentharris5123@yahoo.com>
2017-11-12 07:11:45 -0500

    wants me to commit

--- home/index.php
+++ home/index.php
@@ -23,6 +23,7 @@ if (!$_SESSION['logged_in']) {
 <html lang="en">
     <head>
         <meta charset="utf-8">    
+        <meta http-equiv="refresh" content="160">
         <meta name="viewport" content="width=device-width, initial-scale=1">
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -117,7 +118,7 @@ if (!$_SESSION['logged_in']) {
               <li class="active"><a href="#">Home</a></li>
               <li><a id="buttonAdd">Add</a><li>
               <li><a href="./about/">About</a></li>
-              <li><a href="<?php if ($_SESSION['logged_in']) { echo "../account/profile/"; } ?>">Profile</a></li>
+              <li><a href="<?php echo "../account/profile/" ?>">Profile</a></li>
               <li><a href="<?php echo $href ?>"><?php echo $value ?></a></li>
             </ul>
         </div>
